a04af808ea7a2069d4f6002ccc8f136cf1959b1d
karan7638 <karan2421kumar@gmail.com>
2024-11-14 16:33:35 +0530

    ripple effect btn 1.0

---
+++ script.js
@@ -0,0 +1,12 @@
+const btnEl = document.querySelector(".btn")
+
+btnEl.addEventListener("mouseover", (e)=> {
+    
+
+    const x = e.pageX - btnEl.offsetLeft;
+    const y = e.pageY - btnEl.offsetTop;
+
+    btnEl.style.setProperty("--posX", x +"px")
+    btnEl.style.setProperty("--posY", y + "px")
+
+})
